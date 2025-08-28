631c5fdd7731b167f805a7c5f74709c9c6663d2e
Ella Dunbar <edunbar02@gmail.com>
2025-08-28 13:20:06 -0500

    Add wildtype handling to sleep assay

--- sleep.R
+++ sleep.R
@@ -6,44 +6,53 @@ source("utils.R")
 # assay variable setup
 suffixes <- list(genotypes = "_genotypes.csv", fish_used = "_fish.txt")
 all_files <- find_data("sleep", suffixes)
-main_files <- all_files[[1]]
-wildtype_files <- all_files[[2]]
+main_files <- all_files[["main_files"]]
+wildtype_files <- all_files[["wildtype_files"]]
+wildtype_exists <- length(wildtype_files) != 0
 
-# analysis loop
-all_data <- list()
-for (i in seq_along(main_files)) {
-  # get group
-  group <- main_files[[i]]
+# analysis function
+analyze <- function(files) {
+  analyzed_data <- list()
+  for (prefix_name in names(files)) {
+    # get group
+    group <- files[[prefix_name]]
 
-  # get all requested data
-  main_data <- load_zantiks(group$main, "dcidddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
-  genotypes <- load_genotypes(group$genotypes, group$fish_used, "down")
+    # get all requested data
+    main_data <- load_zantiks(group$main, "dcidddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+    genotypes <- load_genotypes(group$genotypes, group$fish_used, "down")
 
-  long_data <- main_data %>%
-    pivot_longer(
-      cols = A1_Z1:A48_Z2,
-      names_to = c("ARENA", "ZONE"),
-      names_pattern = "A([0-9]+)_Z([1,2])",
-      values_to = "DISTANCE"
-    ) %>%
-    mutate(ARENA = as.integer(ARENA), ZONE = as.integer(ZONE)) %>%
-    relocate(ARENA, ZONE) %>%
-    arrange(ARENA, ZONE)
+    long_data <- main_data %>%
+      pivot_longer(
+        cols = A1_Z1:A48_Z2,
+        names_to = c("ARENA", "ZONE"),
+        names_pattern = "A([0-9]+)_Z([1,2])",
+        values_to = "DISTANCE"
+      ) %>%
+      mutate(ARENA = as.integer(ARENA), ZONE = as.integer(ZONE)) %>%
+      relocate(ARENA, ZONE) %>%
+      arrange(ARENA, ZONE)
 
-  # bin numbers are better than seconds
-  # seconds includes acclimation and tracking time :(
-  hourly_data <- long_data %>%
-    mutate(hour = as.integer(floor((BIN_NUM - 1) / 3300)) + 1) %>%
-    group_by(ARENA, hour) %>%
-    summarise(distance = sum(DISTANCE)) %>%
-    ungroup()
+    # bin numbers are better than seconds
+    # seconds includes acclimation and tracking time :(
+    hourly_data <- long_data %>%
+      mutate(hour = as.integer(floor((BIN_NUM - 1) / 3300)) + 1) %>%
+      group_by(ARENA, hour) %>%
+      summarise(distance = sum(DISTANCE)) %>%
+      ungroup()
 
-  # add to list to combine later
-  all_data[[names(main_files)[i]]] <- hourly_data %>%
-    attach_genotypes(genotypes)
+    # add to list to combine later
+    analyzed_data[[prefix_name]] <- hourly_data %>%
+      attach_genotypes(genotypes)
+  }
 
-  prism_data <- hourly_data %>%
-    attach_genotypes(genotypes) %>%
+  analyzed_data
+}
+
+# save main data files
+main_data <- analyze(main_files)
+for (prefix_name in names(main_files)) {
+  df <- main_data[[prefix_name]]
+  prism_data <- df %>%
     arrange(ARENA, hour) %>%
     select(genotype, ARENA, hour, distance) %>%
     complete(genotype, ARENA = 1:200) %>%
@@ -56,12 +65,29 @@ for (i in seq_along(main_files)) {
     arrange(hour)
 
   # save the data!
-  write_csv(prism_data, file.path("data", "sleep", "output", paste0(names(main_files)[i], ".csv")))
+  write_csv(prism_data, file.path("data", "sleep", "output", paste0(prefix_name, ".csv")))
+}
+
+# handle wildtype data if it exists
+if (wildtype_exists) {
+  # we don't the wildtype data if it's also in our regular data
+  wildtype_only_names <- setdiff(names(wildtype_files), names(main_files))
+  wildtype_data <- analyze(wildtype_files)
+  combined_wildtype <- bind_rows(wildtype_data, .id = "id") %>%
+    filter(genotype == "WT") %>%
+    filter(id %in% wildtype_only_names)
+  combined_main <- bind_rows(main_data, .id = "id")
+
+  all_data <- bind_rows(combined_wildtype, combined_main)
+  all_names <- union(names(wildtype_files), names(main_files))
+} else {
+  all_data <- bind_rows(main_data, .id = "id")
+  all_names <- names(main_files)
 }
 
 # combine all data together
-combined_data <- bind_rows(all_data, .id = "id") %>%
-  mutate(id_num = match(id, names(main_files))) %>% # convert each id (name from list) to int
+combined_data <- all_data %>%
+  mutate(id_num = match(id, all_names)) %>% # convert each id (name from list) to int
   group_by(genotype) %>%
   arrange(id_num, ARENA) %>%
   mutate(unique_id = 96 * (id_num - 1) + ARENA - 1) %>% # both 1-indexed
@@ -77,4 +103,5 @@ combined_data <- bind_rows(all_data, .id = "id") %>%
   select(hour, paste0("WT", 1:200), paste0("HET", 1:200), paste0("HOM", 1:200)) %>%
   arrange(hour)
 
+# save the data
 write_csv(combined_data, file.path("data", "sleep", "output", "combined.csv"))
