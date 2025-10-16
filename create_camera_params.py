abdf18fe9cbee276e1bd822507612d49a04f04f8
Ella Dunbar <emdunbar@uab.edu>
2025-10-16 09:41:20 -0500

    Fix extraneous points for camera parameters

--- create_camera_params.py
+++ create_camera_params.py
@@ -44,7 +44,7 @@ def get_arena_coords(arena_map: NDArray, arena: int) -> NDArray[bool]:
     return np.all(arena_map == arena_color, axis=-1)
 
 
-def bin_data(all_arena_positions: dict[int, NDArray], scale: float = 2.5) -> NDArray:
+def bin_data(all_arena_positions: dict[int, NDArray], scale: float = 3) -> NDArray:
     # plate is 127.76 mm x 85.4 mm
     bins = np.zeros((math.ceil(85.4 * scale), math.ceil(127.76 * scale)))
     for arena_positions in all_arena_positions.values():
@@ -53,11 +53,12 @@ def bin_data(all_arena_positions: dict[int, NDArray], scale: float = 2.5) -> NDA
         y = index_like_positions[:, 1]
         bins[y, x] += 1
     bins = ndimage.gaussian_filter(bins, scale)
+    bins = np.clip(bins, a_min=0.25, a_max=None)
     bins -= np.min(bins)
     return bins
 
 
-def points_in_bin(points: NDArray, bins: NDArray, scale: float = 2.5) -> NDArray:
+def points_in_bin(points: NDArray, bins: NDArray, scale: float = 3) -> NDArray:
     in_bin = np.zeros(points.shape[0]).astype(bool)
     for i, (x, y) in enumerate(points):
         in_bin[i] = binned_data[math.floor(y * scale), math.floor(x * scale)] > 0
@@ -108,6 +109,8 @@ arena_type_to_num_arenas = {
     "ymaze_4":            4,
     "ymaze_15":          15,
 }
+# some arena types end up with degenerate coordinates in certain
+# directions. we should ignore those directions when matching points
 arena_type_to_bad_directions = {
     "48_well":           set(),
     "12_well":           set(),
@@ -170,9 +173,14 @@ for arena_type, assay_types in arena_to_assay_type.items():
     used_arenas_indices = []
     used_arenas = np.zeros((asset_map.shape[0], asset_map.shape[1]))
     for i in range(arena_type_to_num_arenas[arena_type]):
-        if len(arena_points[i + 1]) > 50:
+        if len(arena_points[i + 1]) > 1000: # need > 1000 points to make a good map
             used_arenas_indices.append(i + 1)
             used_arenas += get_arena_coords(asset_map, i + 1)
+    plt.figure(dpi=175)
+    plt.imshow(binned_data, cmap="magma")
+    plt.axis("off")
+    plt.title(f"Binned Data for {arena_type}")
+    plt.show()
 
     # get cardinal points for each arena
     good_directions = [
