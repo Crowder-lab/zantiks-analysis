1cec196799d18b05e727244ec2c1ce7ceaf0b581
Ella Dunbar <emdunbar@uab.edu>
2025-10-20 14:58:44 -0500

    Include heatmaps in run all script

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# regular analyses
+./developmental_delay.py
 ./distance_travelled.R
 ./light_dark_preference_3wpf.R
 ./light_dark_preference_6dpf.R
@@ -10,3 +12,15 @@
 ./startle_response.R
 ./ymaze_15.R
 ./ymaze_4.R
+
+# heatmaps
+./distance_travelled_heatmap.py
+./light_dark_preference_3wpf_heatmap.py
+./light_dark_preference_6dpf_heatmap.py
+./light_dark_transition_heatmap.py
+./mirror_biting_heatmap.py
+./sleep_heatmap.py
+./social_preference_heatmap.py
+./startle_response_heatmap.py
+./ymaze_15_heatmap.py
+./ymaze_4_heatmap.py
